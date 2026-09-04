6be229f78625d300032b7aecd7a1c54a890907b2
Yuri Melo <[200~177472125+YuriMeloP@users.noreply.github.com~>
2026-09-04 15:26:28 -0300

    Navegar entre a lista e cadastro

--- main.py
+++ main.py
@@ -1,52 +1,101 @@
 import tkinter as tk
 
 janela = tk.Tk()
+
 janela.title("BookTrack")
 janela.geometry("700x500")
 janela.configure(bg="#1a1919")
-titulo = tk.Label(
+
+titulo_app = tk.Label(
     janela,
     text="BookTrack",
     font=("Arial", 24, "bold"),
     bg="#1a1919",
     fg="white"
 )
-titulo.pack(pady=15)
+
+titulo_app.pack(pady=15)
+
+frame_cadastro = tk.Frame(
+    janela,
+    bg="#1a1919"
+)
+
+frame_cadastro.pack()
+
+frame_lista = tk.Frame(
+    janela,
+    bg="#1a1919"
+)
 
 label_titulo = tk.Label(
-    janela,
+    frame_cadastro,
     text="Livro:",
     bg="#1a1919",
     fg="white"
 )
+
 label_titulo.pack()
 
-entrada_titulo = tk.Entry(janela)
+entrada_titulo = tk.Entry(frame_cadastro)
 entrada_titulo.pack()
 
 label_autor = tk.Label(
-    janela,
+    frame_cadastro,
     text="Autor:",
     bg="#1a1919",
     fg="white"
 )
+
 label_autor.pack()
 
-entrada_autor = tk.Entry(janela)
+entrada_autor = tk.Entry(frame_cadastro)
 entrada_autor.pack()
 
 def adicionar_livro():
-    titulo = entrada_titulo.get()
+    nome_livro = entrada_titulo.get()
     autor = entrada_autor.get()
 
-    print(f"{titulo} - {autor}")
+    livro = f"{nome_livro} - {autor}"
+
+    lista_livros.insert(tk.END, livro)
+
+def ver_lista():
+    frame_cadastro.pack_forget()
+    frame_lista.pack()
+
+def voltar():
+    frame_lista.pack_forget()
+    frame_cadastro.pack()
 
 botao_adicionar = tk.Button(
-    janela,
+    frame_cadastro,
     text="Adicionar livro",
     command=adicionar_livro
 )
+
+botao_ver_lista = tk.Button(
+    frame_cadastro,
+    text="Ver lista",
+    command=ver_lista
+)
+
+botao_voltar = tk.Button(
+    frame_lista,
+    text="Voltar",
+    command=voltar
+)
+
+botao_voltar.pack()
+botao_ver_lista.pack()
 botao_adicionar.pack(pady=10)
 
-janela.mainloop()
+lista_livros = tk.Listbox(
+    frame_lista,
+    width=50,
+    height=10
+)
 
+lista_livros.pack()
+
+janela.mainloop()
